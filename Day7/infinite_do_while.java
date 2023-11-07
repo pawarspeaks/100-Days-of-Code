f116ade0eb84e97101593082ed270bdc5ca89319
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2023-11-07 17:59:05 +0530

    Add files via upload

---
+++ Day7/infinite_do_while.java
@@ -0,0 +1,8 @@
+public class infinite_do_while {
+       public static void main(String args[]) {
+      do {
+        System.out.println("hello this is an infinite do while loop➿");
+
+      } while(true);
+   }   
+}
