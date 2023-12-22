b0db07485b24fc51bce3676212118fb5b29c2566
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2023-12-22 21:29:02 +0530

    Create Hackerrank_Java.java

---
+++ Day_52/Hackerrank_Java.java
@@ -0,0 +1,24 @@
+// Question link: https://www.hackerrank.com/challenges/java-md5
+
+import java.util.Scanner;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Solution {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        /* Read and save the input String */
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        scan.close();
+        
+        /* Encode the String using MD5 */
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(str.getBytes());
+        byte[] digest = md.digest();
+        
+        /* Print the encoded value in hexadecimal */
+        for (byte b : digest) {
+            System.out.format("%02x", b);
+        }
+    }
+}￼Enter
