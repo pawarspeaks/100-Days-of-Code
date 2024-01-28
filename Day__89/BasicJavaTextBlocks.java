b49a8c3c66a5d8a23eb5fb7f7770343503949349
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2024-01-28 23:36:44 +0530

    Create BasicJavaTextBlocks.java

---
+++ Day__89/BasicJavaTextBlocks.java
@@ -0,0 +1,14 @@
+public class BasicJavaTextBlocks {
+    public static void main(String[] args) {
+        // Using text blocks for multiline strings
+        String multilineString = """
+            This is a multiline
+            text block in Java.
+            It allows for easy
+            formatting and readability.
+            """;
+
+        // Printing the multiline string
+        System.out.println(multilineString);
+    }
+}
