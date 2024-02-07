422c2aecd7bf01d12ed6ef091b78386b5d8a6505
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2024-02-07 23:21:50 +0530

    Create BasicCodeSnippetsInAPIDocs.java

---
+++ Day__99/BasicCodeSnippetsInAPIDocs.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class BasicCodeSnippetsInAPIDocs {
+    public static void main(String[] args) {
+        // Creating an ArrayList
+        ArrayList<String> list = new ArrayList<>();
+
+        // Adding elements to the ArrayList
+        list.add("Java");
+        list.add("Code");
+        list.add("Snippets");
+
+        // Displaying the elements
+        System.out.println("ArrayList: " + list);
+    }
+}
